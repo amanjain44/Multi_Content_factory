8333cf20a741d6cc32ea6355b9aca1524cdee838
Aman Jain <amanjainj938@gmail.com>
2026-09-23 20:18:03 +0530

    Allow all CORS origins to fix Vercel fetch errors

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,8 +11,8 @@ app = FastAPI(
 # Set all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
